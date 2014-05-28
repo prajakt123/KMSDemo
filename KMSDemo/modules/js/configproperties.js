9405cecab4aa02f0c5e37c3c1ecbdbc8406458ce
Bhargavi Burugupalli <bhargavi.burugupalli@kony.com>
2014-05-28 14:37:23 -0400

    Update configproperties.js

--- KMSDemo/modules/js/configproperties.js
+++ KMSDemo/modules/js/configproperties.js
@@ -2,10 +2,10 @@
 // if using diff server from ur cloud input here else get teh url from appconfig
 var KMSPROP = {
 
-		"kmsserverurl" : "https://konysb2.messaging.konycloud.com",
-		"senderID" :"718533424661",
-		"appID":"KMSDemo",
+		"kmsserverurl" : "<kms cloud console url>",
+		"senderID" :"<senderID>", // applicabel for android pnly
+		"appID":"KMSDemo", // appID generated or given int he cloud KMS console
 		"deviceID" : kony.os.deviceInfo().deviceid,
-		"ufID":"kmsdemouser@kony.com"
+		"ufID":"kmsdemouser@kony.com" // any unique identifier for the app
 		
 }
